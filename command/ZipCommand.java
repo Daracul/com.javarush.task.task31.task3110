081d5b306e6134dfed2104b667835529edbc5705
Alexey Malakhov <au.malakhov@gmail.com>
2017-05-26 16:44:50 +0300

    Archiver, step 5

---
+++ command/ZipCommand.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task31.task3110.command;
+
+import com.javarush.task.task31.task3110.command.Command;
+
+/**
+ * Created by amalakhov on 26.05.2017.
+ */
+public abstract class ZipCommand implements Command {
+
+
+}
