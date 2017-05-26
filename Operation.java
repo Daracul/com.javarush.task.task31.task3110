c2ea7cf28f99078a5e20ecd3738485973b9e3fc1
Alexey Malakhov <au.malakhov@gmail.com>
2017-05-26 16:23:59 +0300

    Archiver, step 3

---
+++ Operation.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task31.task3110;
+
+/**
+ * Created by amalakhov on 26.05.2017.
+ */
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
